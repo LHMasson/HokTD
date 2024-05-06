082ff5a068c9e8a2090d7f36d38951984ac5c571
LHMasson <lucas.masson001@gmail.com>
2024-05-05 21:52:38 -0300

    Fix variable names

--- src/test/java/solver/solverTest.java
+++ src/test/java/solver/solverTest.java
@@ -9,8 +9,8 @@ public class solverTest {
     public static void main(String[] args) {
         Board board = InputInstance.getInstance("src/test/java/files/inst03.in");
         State initialState = new State(board);
-        StateSpaces ee = new StateSpaces(initialState, new Pile());
-        State solucao = ee.solve();
-        System.out.println(solucao.toString());
+            StateSpaces stateSpaces = new StateSpaces(initialState, new Pile());
+        State solution = stateSpaces.solve();
+        System.out.println(solution.toString());
     }
 }
